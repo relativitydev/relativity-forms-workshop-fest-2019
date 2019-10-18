359f16f9ec2e189536be1bff465944604cc5a73e
ObviouslyWorking <35307977+obviouslyworking@users.noreply.github.com>
2019-10-18 12:48:29 -0500

    Cleaning up the repo - removal of superfluous code, alteration of exercise code

--- source/RelativityFormsWorkshop/RelativityForms/src/relativityFormsConversion.js
+++ source/RelativityFormsWorkshop/RelativityForms/src/relativityFormsConversion.js
@@ -1,29 +1,21 @@
-(function iifeGeneratingEventHandlers(eventNames, convenienceApi, privilegedEnvelope) {
-	/* global alert, eventNames, convenienceApi, privilegedEnvelope */
-
-	// using "privilegedEnvelope" gives us exposure of internals at test-time, without
-	// exposing them in production.  This makes it possible to test our code a in a more
-	// unitary way than we would otherwise be able, due to RelativityForms handler code
-	// being defined within an immediately invoked function expression
-	var vars = privilegedEnvelope || {};
-
+(function iifeGeneratingEventHandlers(eventNames, convenienceApi) {
 	// the object we will be returning
 	var eventHandlers = {};
 
 	// Field GUIDs
-	vars.CURRENT_ENTHUSIASM_GUID = "692C7C4D-3C6B-4729-86E4-426A101E1479".toLowerCase();
-	vars.MAXIMUM_ENTHUSIASM_GUID = "5426056E-C815-430C-93FB-57C979A88715".toLowerCase();
-	vars.CONTAIN_ENTHUSIASM_GUID = "0EE79570-5540-49F9-8DED-BDE5840AF433".toLowerCase();
+	var CURRENT_ENTHUSIASM_GUID = "692C7C4D-3C6B-4729-86E4-426A101E1479".toLowerCase();
+	var MAXIMUM_ENTHUSIASM_GUID = "5426056E-C815-430C-93FB-57C979A88715".toLowerCase();
+	var CONTAIN_ENTHUSIASM_GUID = "0EE79570-5540-49F9-8DED-BDE5840AF433".toLowerCase();
 
 	// skeleton console creation handler - even with it being empty like it is below
 	// the existence of the function will tell Relativity Forms to create the console
 	// in View mode.
 	eventHandlers[eventNames.CREATE_CONSOLE] = function () {
-		// not doing much
+		// We're not doing much here. This will be fleshed-out in exercise 4.
 		return;
 	};
 
 	return eventHandlers;
-}(eventNames, convenienceApi, privilegedEnvelope));
+}(eventNames, convenienceApi));
 
 // eof
--- source/WorkshopSolutions/Exercise 2 - Automation/ReadMe.txt
+++
@@ -1,6 +0,0 @@
-
-* Updated relativityFormsConversion.js
-	- No longer failing linting
-	- No longer failing tests
-	- Exposes a handler for CREATE_CONSOLE
-* PIEH is updated to implement RF
